628da1bfa64595d8096bfcaf84a8a235000884d6
shin <sars21@creativelab-inc.com>
2024-01-11 00:47:23 +0900

    start

---
+++ src/main/java/shin/com/show_voucher/ShowVoucherApplication.java
@@ -0,0 +1,15 @@
+package shin.com.show_voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowVoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowVoucherApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/shin/com/show_voucher/ShowVoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package shin.com.show_voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowVoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
